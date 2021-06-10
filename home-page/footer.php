b1729860e4f33d6f8b70ed299f558442c89389bd
Nguyen Ngoc Ha <38517373+theforec@users.noreply.github.com>
2021-06-10 22:40:59 +0700

    create

---
+++ home-page/footer.php
@@ -0,0 +1,54 @@
+
+    <footer class="light-gray-bg">
+      <div class="container"> 
+        
+        <!-- Social Links -->
+        <ul class="social-link margin-top-0 margin-bottom-80 text-center animate fadeInUp" data-wow-delay="0.4s">
+          <li><a href="#.">Facebook</a></li>
+          <li><a href="#.">Google+</a></li>
+          <li><a href="#.">Twitter</a></li>
+          <li><a href="#.">Behance</a></li>
+          <li><a href="#.">Dribbble</a></li>
+          <li><a href="#.">Pinterest</a></li>
+          <li><a href="#.">Instagram</a></li>
+        </ul>
+        
+        <!-- Footer Row -->
+        <div class="row"> 
+          
+          <!-- Left Section -->
+          <div class="col-sm-4 animate fadeInLeft" data-wow-delay="0.4s">
+            <h6>KORA STUDIO</h6>
+            <p>35 Elm St<br>
+              Rugaong, 12569<br>
+              Dublin</p>
+            <a href="#." class="btn">Locate on map</a> </div>
+          <div class="col-sm-4 margin-top-30 text-center animate fadeInUp" data-wow-delay="0.4s" > <img src="images/footer-logo.png" alt=""> </div>
+          
+          <!-- Right Section -->
+          <div class="col-sm-4 text-right animate fadeInRight" data-wow-delay="0.4s">
+            <h6>Contact Info</h6>
+            <p>P: 123 4569 789- 95<br>
+              E: hello@kora.com<br>
+              sales@kora.com</p>
+            <a href="#." class="btn">Write to Us</a> </div>
+        </div>
+      </div>
+    </footer>
+  <!-- </div>
+</div> -->
+<!-- Wrap End --> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<script src="js/wow.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/own-menu.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/jquery.cubeportfolio.min.js"></script> 
+<script src="js/counter.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
