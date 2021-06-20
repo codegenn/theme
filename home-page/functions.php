d057ba19df8e09f557acc79ee341e5fa2a8192d0
Nguyễn Cẩm <hongcam941997@gmail.com>
2021-06-20 10:36:49 +0700

    Show data service

--- home-page/functions.php
+++ home-page/functions.php
@@ -19,18 +19,18 @@ add_action('wp_enqueue_scripts', 'b2w_them_style');
 function add_js()
 {
     $theme_url = get_template_directory_uri();
-?>
-    <script src=<?php echo $theme_url . '/assets/js/jquery-1.11.3.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/bootstrap.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/counter.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/jquery.cubeportfolio.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/main.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/modernizr.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/owl.carousel.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/js/wow.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/rs-plugin/js/jquery.themepunch.tools.min.js' ?>></script>
-    <script src=<?php echo $theme_url . '/assets/rs-plugin/js/jquery.themepunch.revolution.min.js' ?>></script>
-<?php
+    ?>
+        <script src=<?php echo $theme_url . '/assets/js/jquery-1.11.3.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/bootstrap.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/counter.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/jquery.cubeportfolio.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/main.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/modernizr.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/owl.carousel.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/js/wow.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/rs-plugin/js/jquery.themepunch.tools.min.js' ?>></script>
+        <script src=<?php echo $theme_url . '/assets/rs-plugin/js/jquery.themepunch.revolution.min.js' ?>></script>
+    <?php
 }
 add_action('wp_footer', 'add_js');
 
@@ -53,3 +53,32 @@ function add_link_atts($atts)
 }
 
 add_filter('nav_menu_link_attributes', 'add_link_atts');
+
+function service_item() {
+    $url = get_template_directory_uri() . "/data-services/services.json";
+    $string = file_get_contents($url);
+    echo '<div class="my_class">'. $string .'</div>';
+    if ($string === false) {
+        echo '<div class="my_class"> fasle 1</div>';
+    }
+    $json_a = json_decode($string, true);
+    if ($json_a === null) {
+        echo '<div class="my_class"> fasle 2</div>';
+    }
+    foreach ($json_a as $item) {
+        echo '
+            <div class="cbp-item photo pack">
+                <img src=' . get_template_directory_uri() . $item['image'] . ' alt="Image ' . $item['title'] . '">
+                <div class="item-description">
+                    <div class="d-flex flex-column">
+                        <p><strong>' . $item['title']       . '</strong></p>
+                        <p>'         . $item['description'] . '</p>
+                        <p><strong>' . $item['price']       . '</strong></p>
+                    </div>
+                </div>
+            </div>
+        ';
+    }
+}
+
+?>
--- home-page/service/portfolio.php
+++ home-page/service/portfolio.php
@@ -1,51 +1,7 @@
-<!-- PORTFOLIO -->
 <section class="portfolio padding-top-100">
-
-    <!-- PORTOFLIO ITEMS -->
     <div class="container">
         <div id="js-grid-awesome-work" class="with-space col-3">
-
-            <?php
-
-            $string = file_get_contents(get_template_directory_uri() . "/data-services/services.json");
-            if ($string === false) {
-                echo '<div class="my_class">' . $cart->count_product . '</div>';
-            }
-
-            $json_a = json_decode($string, true);
-            if ($json_a === null) {
-                // deal with error...
-                echo '<div class="my_class"> fasle 2</div>';                // deal with error...
-            }
-
-            foreach ($json_a as $dataItems => $item) {
-                // print_r($person_a['status']);
-                echo '<div class="cbp-item photo pack">
-                <img src=' . get_template_directory_uri() . $item['image'] . ' alt="">
-                <div class="item-description">
-                    <div class="d-flex flex-column">
-                        <p><strong>' . $item['title'] . '</strong></p>
-                        <p>' . $item['description'] . '</p>
-                        <p><strong>' . $item['price'] . '</strong></p>
-                    </div>
-                </div>
-            </div>';                // deal with error...
-            }
-
-            ?>
-
-            <!-- <div class="cbp-item photo pack">
-                <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/2/img-1.jpg" ?> alt="">
-                <div class="item-description">
-                    <div class="d-flex flex-column">
-                        <p><strong>IMAGE ENCHANCEMENT</strong></p>
-                        <p>Perfect for real estate photo editing so you can present your listing with top-notch, retouched
-                            professional - looking photos</p>
-                        <p><strong>US$1.60</strong></p>
-                    </div>
-                </div>
-            </div> -->
+            <?php service_item() ?>
         </div>
     </div>
-
 </section>
