52070c6ed119a5673358ee5031bf91e1a5dfc8bb
Nguyen Ngoc Ha <38517373+theforec@users.noreply.github.com>
2021-06-27 22:59:12 +0700

    add pages

---
+++ home-page/data-services/service-detail-4/portfolio.php
@@ -0,0 +1,111 @@
+<!-- PORTFOLIO -->
+<section class="portfolio padding-top-50 padding-bottom-50 animate fadeInUp" data-wow-delay="0.4s">
+    <div class="container">
+        <!-- Tittle -->
+        <h2 class="simple-tittle margin-bottom-50">ARTWORK</h2>
+    </div>
+
+    <!-- PORTOFLIO ITEMS -->
+    <div id="js-grid-awesome-work" class="col-4">
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-1.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project1.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-2.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project2.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-3.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project3.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-4.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project4.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-5.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project5.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-7.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project6.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-6.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project7.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- PORTOFLIO ITEM -->
+        <div class="cbp-item"> <img src=<?php echo get_template_directory_uri() . "/assets/images/portfolio/1/img-8.jpg" ?> alt="">
+            <div class="hover-port">
+                <div class="hover-in">
+                    <div class="position-center-center full-width">
+                        <h6>EMPATIA STUDIO</h6>
+                        <span>Branding, Identity, Stationary</span> <a href="ajax-projects/project8.html" class="cbp-singlePage" rel="nofollow"><i class="fa fa-link"></i></a> <a href="images/portfolio/1/img-3.jpg" class="cbp-lightbox" data-title=""><i class="ion-ios-search-strong"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- LOAD MORE -->
+    <div id="js-loadMore-awesome-work" class="text-center"> <a href="ajax-projects/loadMore.html" class="cbp-l-loadMore-link btn text-center" rel="nofollow"> <span class="cbp-l-loadMore-defaultText">View
+                More</span> <span class="cbp-l-loadMore-loadingText">Please Wait (Loading...</span> <span class="cbp-l-loadMore-noMoreLoading">No More Work Avaiable</span> </a> </div>
+</section>
---
+++ home-page/data-services/service-detail-6/index.php
@@ -0,0 +1,10 @@
+<?php
+/* Template Name: Service Detail Page */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php get_header(); ?>
+<?php include get_template_directory() . '/data-services/service-detail-6/body.php'; ?>
+<?php get_footer(); ?>
+
+</html>
