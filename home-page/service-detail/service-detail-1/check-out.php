19be5f593a592149c6e5f8023661d854f1c9b10f
Nguyễn Cẩm <hongcam941997@gmail.com>
2021-06-28 19:30:16 +0700

    add page detail

--- home-page/service-detail/service-detail-1/check-out.php
+++ home-page/service-detail/service-detail-1/check-out.php
@@ -5,7 +5,31 @@
 <section class="container margin-b-40" style="height: 571px;">
     <div class="tp-banner-container">
         <div class="tp-banner">
-            <?php service_detail_item("/data-services/services_detail_1.json") ?>
+            <?php service_detail_item("/data-services/service_detail_1.json") ?>
         </div>
     </div>
 </section>
+
+<?php
+    function service_detail_item($url_file) {
+        $url = get_template_directory_uri() . $url_file;
+        $string = file_get_contents($url);
+        if ($string === false) {
+            echo '<div class="my_class"> fasle 1</div>';
+        }
+        $json_a = json_decode($string, true);
+        if ($json_a === null) {
+            echo '<div class="my_class"> fasle 2</div>';
+        }
+    
+        $string_ul = '<ul>';
+        $string_li = '';
+    
+    
+        foreach ($json_a as $item) {
+            $string_li .= '<li data-transition="fade" data-slotamount="5" data-masterspeed="700"> <img src='. $item['image'] .' ?> alt="" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat"></li>';
+        };
+    
+        echo $string_ul.$string_li.'</ul>';
+    }
+?>
