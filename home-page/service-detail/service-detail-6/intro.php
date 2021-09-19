aca2ab04d9f4e952b1123076c194e02149db7fee
Cam.nguyenhong <hongcam941997@gmail.com>
2021-09-19 10:13:04 +0700

    test....

--- home-page/service-detail/service-detail-6/intro.php
+++ home-page/service-detail/service-detail-6/intro.php
@@ -2,7 +2,7 @@
 <section class="intro padding-top-100">
     <div class="container">
         <div class="choose-us intro-in animate fadeInUp" data-wow-delay="0.4s">
-            <h3 class="padding-top-60 padding-bottom-60"><strong>SINGLE + MULTIPLE EXPOSURE BLENDING?</strong></h3>
+            <h3 class="padding-top-60 padding-bottom-60"><strong>Video</strong></h3>
             <div class="row">
                 <!-- Check item -->
                 <div class="col-md-6 animate fadeInUp" data-wow-delay="0.4s">
@@ -13,10 +13,11 @@
                             </svg>
                         </div>
                         <div class="d-flex flex-column text-left">
-                            <h4><strong>BLACK LIVING ROOMS IDEAS</strong></h4>
+                            <h4><strong>ENHANCE YOUR VIRTUAL TOURS</strong></h4>
                             <p class="bd width-40 margin-t-10 margin-b-20"></p>
-                            <span>Sed us perpiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium totam rem</span>
+                            <span>We can enhance any 360° image to make them look high quality and professional.</span>
+                            </br>
+                            <span>Traditionally 360° images are low quality, grainy and dark. We have the only technology available to enhance all types of 360° Images.</span>
                         </div>
                     </div>
                 </div>
@@ -30,10 +31,9 @@
                             </svg>
                         </div>
                         <div class="d-flex flex-column text-left">
-                            <h4><strong>CEILINGS AND WOOD FLOORS</strong></h4>
+                            <h4><strong>SELL PROPERTIES 50% FASTER</strong></h4>
                             <p class="bd width-40 margin-t-10 margin-b-20"></p>
-                            <span>Sed us perpiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium totam rem</span>
+                            <span>360° images provide deeper engagement with property buyers. An increasingly popular medium for allowing buyers to interact with a home in a way single images never can.</span>
                         </div>
                     </div>
                 </div>
@@ -47,10 +47,9 @@
                             </svg>
                         </div>
                         <div class="d-flex flex-column text-left">
-                            <h4><strong>ARTISTIC APARTMENTS</strong></h4>
+                            <h4><strong>FAST TURN AROUND</strong></h4>
                             <p class="bd width-40 margin-t-10 margin-b-20"></p>
-                            <span>Sed us perpiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium totam rem</span>
+                            <span>Receive your edited 360° Images back within 24 hours, ready to upload to your website or MLS.</span>
                         </div>
                     </div>
                 </div>
@@ -64,10 +63,24 @@
                             </svg>
                         </div>
                         <div class="d-flex flex-column text-left">
-                            <h4><strong>ARTISTIC APARTMENTS</strong></h4>
+                            <h4><strong>HOME BUYERS WANT TO SEE VIRTUAL TOURS</strong></h4>
                             <p class="bd width-40 margin-t-10 margin-b-20"></p>
-                            <span>Sed us perpiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium totam rem</span>
+                            <span>360° Images are a great Real Estate marketing tool, but are generally low quality and don’t show the home in the best light. We can now enhance all formats of 360° Images to make them like you’re standing in the room.</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 animate fadeInUp" data-wow-delay="0.4s">
+                    <div class="d-flex">
+                        <div class="padding-10 margin-r-10 check-color">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
+                            </svg>
+                        </div>
+                        <div class="d-flex flex-column text-left">
+                            <h4><strong>MORE ONLINE VIEWS</strong></h4>
+                            <p class="bd width-40 margin-t-10 margin-b-20"></p>
+                            <span>Users spend 52% longer on listings featuring a virtual tour; Properties with virtual tours have 80% more enquires than those without.</span>
                         </div>
                     </div>
                 </div>
